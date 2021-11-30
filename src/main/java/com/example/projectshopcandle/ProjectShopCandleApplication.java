973c8a18f9121a1e8f6df2eb22fba1c55828d724
1-THAODINH-L\thao.dinh_onemount <thaodphe141294@fpt.edu.vn>
2021-11-30 15:37:09 +0700

    base

---
+++ src/main/java/com/example/projectshopcandle/ProjectShopCandleApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectshopcandle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectShopCandleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectShopCandleApplication.class, args);
+    }
+
+}
